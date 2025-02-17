f25f20b3f405ed6501bdbb5007eb4dff4f78e4a6
franckyy <franck.calestroupat@protonmail.com>
2025-02-17 12:40:22 +0100

    Génération des données d'entraînement, paramétrage du nombre d'époques pour que le système converge

--- LinearRegression/src/main/java/fr/deepLearning_02/LinearRegression/LinearRegressionExample.java
+++ LinearRegression/src/main/java/fr/deepLearning_02/LinearRegression/LinearRegressionExample.java
@@ -8,7 +8,7 @@ import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.learning.config.Adam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class LinearRegressionExample {
@@ -16,8 +16,8 @@ public class LinearRegressionExample {
 		// Définir un réseau neuronal simple avec une seule couche
 		MultiLayerNetwork model = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
 				.seed(123)
-				.weightInit(WeightInit.XAVIER)
-				.updater(new Nesterovs(0.01))	// Utilisation correcte du taux d'apprentissage
+				.weightInit(WeightInit.NORMAL)
+				.updater(new Adam(0.001))	// Utilisation correcte du taux d'apprentissage
 				.list()
 				.layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)	// Mean Squared Error
 						.activation(Activation.IDENTITY)	//activation linéaire
@@ -28,15 +28,23 @@ public class LinearRegressionExample {
 		
 		model.init();
 		
+		//génération de données d'entraînement
+		double[] inTrain = new double[100];
+		double[] labelTrain = new double[100];
+		for (int i = 0; i < 100; i++) {
+		    inTrain[i] = i;
+		    labelTrain[i] = 2 * i + 1;  // Toujours basé sur y = 2x + 1
+		}
+		
 		// Données d'entraînement : x = {1, 2, 3, 4}, y = {3, 5, 7, 9}
         // Correspond à y = 2 x + 1
-		INDArray input = Nd4j.create(new double[] {1, 2, 3, 4}, new int[] {4, 1});
-		INDArray labels = Nd4j.create(new double[] {3, 5, 7, 9}, new int[] {4, 1});
+		INDArray input = Nd4j.create(inTrain, new int[] {100, 1});
+		INDArray labels = Nd4j.create(labelTrain, new int[] {100, 1});
 		
 		DataSet dataSet = new DataSet(input, labels);
 		
 		//Entraînement
-		for(int i = 0; i < 2000; i++) {
+		for(int i = 0; i < 5000; i++) {
 			model.fit(dataSet);
 		    if (i % 500 == 0) {
 		        System.out.println("Époque " + i + " - Score: " + model.score());
@@ -50,6 +58,9 @@ public class LinearRegressionExample {
 		INDArray weights = model.getParam("0_W");
 		INDArray bias = model.getParam("0_b");
 		
+		System.out.println("Input shape: " + input.shapeInfoToString());
+		System.out.println("Labels shape: " + labels.shapeInfoToString());
+
 		// Afficher les résultats
         System.out.println("Prédiction pour x=10 : " + output.getDouble(0));
         System.out.println("Prédiction pour x=100 : " + output.getDouble(1));
